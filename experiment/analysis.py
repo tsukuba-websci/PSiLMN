388dedd15ddc34660f1388da62c70c90527809b5
Alexandre Gournail <alexandre.gournail@grenoble-inp.org>
2024-03-13 11:14:48 +0900

    add: opinion change graphs and accurracy comparison graph

--- experiment/analysis.py
+++ experiment/analysis.py
@@ -13,6 +13,53 @@ from lib.agent import parse_response_mmlu
 import networkx as nx
 import matplotlib.pyplot as plt
 import imageio
+import seaborn as sns
+
+GRAPH_TYPE = ['fully_connected_network',
+            'scale_free_network',
+            'random_network',
+            'watts_strogatz_network']
+
+# class OpinionEvolution:
+#     """ 
+#     Data structure to save an agent opinion between two round.
+#     Round in [2, 3].
+#     """
+#     def __init__(self, previous : str, new : str, correct : str, round : int, agent_num: int) -> None:
+#         self.previous_opinion = previous
+#         self.new_opinion = new
+#         self. correct_response = correct
+#         self.round = round
+#         self.agent = agent_num
+
+#     def isAGoodChange(self) -> bool:
+#         return self.response == self.new_opinion
+
+class Analyse:
+    """Meta data for a given graph and size"""
+
+    def __init__(self, csv_path: str) -> None:
+        # Type is a int, it is easier to compare
+        self.type = file_name = GRAPH_TYPE.index(csv_path.split('/')[2])
+        self.num_agents = int(csv_path.split('/')[3].split('.')[0])
+        self.path = csv_path
+        self.__accuracy = None
+
+    def setAccuracy(self) -> None:
+        self.__accuracy = calculate_accuracy(self.path)
+
+
+    def getAccuracy(self) -> int:
+        # We check if it as already been computed
+        if not self.__accuracy:
+            self.setAccuracy()
+        return self.__accuracy
+
+    def ParseOpinionEvolution(self) -> pd.DataFrame :
+        return find_evolutions(self.path)
+
+    def plotDynamicEvolution(self) -> None :
+        created_figs(self.path)
 
 def created_figs(file_path: str) -> None:
 
@@ -24,7 +71,7 @@ def created_figs(file_path: str) -> None:
     network_type = file_path.split('/')[2]
     num_agents = file_path.split('/')[3].split('.')[0]
 
-    graph = nx.read_graphml(f'data/{network_type}/{num_agents}.graphml')
+    graph = nx.read_graphml(f'experiment/data/{network_type}/{num_agents}.graphml')
     pos = nx.spring_layout(graph)
 
     rounds = df['round'].unique()
@@ -100,29 +147,106 @@ def calculate_accuracy(file_path):
     accuracy_percentage = (correct_answers / total_questions) * 100
     return accuracy_percentage
 
+def find_evolutions(file_path) -> pd.DataFrame :
+    opinion_evol_list = [] # list to be turned into a dataframe
+    df = pd.read_csv(file_path, delimiter='|')
+
+    df['parsed_response'] = df['response'].apply(parse_response_mmlu)
+    df['correct'] = df['parsed_response'] == df['correct_response']
+
+    for id in df['agent_id'].unique():
+        for question in df['question_number'].unique():
+            for round in [0,1]:
+                prev = df.query(f'agent_id == {id} & question_number == {question} & round == {round}')
+                assert len(prev.axes[0]) == 1
+                next = df.query(f'agent_id == {id} & question_number == {question} & round == {round+1}')
+                assert len(next.axes[0]) == 1
+
+                # Here we determine wich type of evolution we have : 00, 01, 10 or 11,
+                # with 0 wrong answer and 1 the good one (01 = from 1 to 0).
+                type = None
+                if bool(prev['correct'].values[0]):
+                    type = "1"
+                else :
+                    type = "0"
+                if bool(next['correct'].values[0]):
+                    type = type+"1"
+                else :
+                    type = type+"0"
+                opinion_evol_list.append([id, str(round + 1), type])
+    return pd.DataFrame(columns = ['agent_id', 'round', 'type'], data=opinion_evol_list)   
+
 if __name__ == "__main__":
     
     # Create the new directory structure if it does not exist
-    output_analyse_path = Path("output/analysis/")
-    output_analyse_path.mkdir(parents=True, exist_ok=True)
-        
+    output_analyse_path = "output/analysis/"
+    Path(output_analyse_path).mkdir(parents=True, exist_ok=True)
+
     # Specify the path to your CSV file
     csv_files = glob.glob('output/agent_responses/**/*.csv', recursive=True)
 
+    # analyses[i] contains the analyses relatives to a graph of type i
+    analyses = [None] * len(GRAPH_TYPE)
+
     # Print the list of files
     for file in csv_files:
+        print(file)
+        current_analyse = Analyse(file) 
         
-        created_figs(file)
+        # created_figs(file)
 
-        file_name = os.path.basename(file).split('.')[0]
-        network_type = file.split('/')[2]
+        # # Calculate the accuracy of group responses
+        # accuracy = current_analyse.getAccuracy()
 
-        # Calculate the accuracy of group responses
-        accuracy = calculate_accuracy(file)
+        # # # save accurracy
+        # with open('output/analysis/accuracy.csv', 'a', newline='') as result_file:
+        #     writer = csv.writer(result_file)
+        #     # Check if the file is empty to write headers
+        #     if result_file.tell() == 0:
+        #         writer.writerow(['graph_type', 'size', 'accuracy'])
+        #     writer.writerow([GRAPH_TYPE[current_analyse.type], current_analyse.num_agents, accuracy])
 
-        with open('output/analysis/results.csv', 'a', newline='') as file:
-            writer = csv.writer(file)
-            # Check if the file is empty to write headers
-            if file.tell() == 0:
-                writer.writerow(['file_name', 'accuracy'])
-            writer.writerow([f"{network_type}_{file_name}", accuracy])  
+        # Parse the opinion evolution list and display it
+        opinion_evol = current_analyse.ParseOpinionEvolution()
+        plt.figure()
+        sns.histplot(data=opinion_evol, x="round", hue="type", multiple="dodge", shrink=.8, stat="count")
+        plt.grid()
+        plt.xlabel("round number")
+        plt.ylabel("number of agents")
+        plt.title(f"opinion changes during the round for {current_analyse.num_agents} agents \nin a {GRAPH_TYPE[current_analyse.type]}")
+
+        # Save the fig
+        file_path = f'{output_analyse_path}/opinion_changes/{GRAPH_TYPE[current_analyse.type]}/'
+        Path(file_path).mkdir(parents=True, exist_ok=True)
+        plt.savefig(Path(f'{file_path}/{current_analyse.num_agents}.png'))
+        plt.close()
+
+        # # add analyse to the list for later
+        # if not (analyses[current_analyse.type]):
+        #     analyses[current_analyse.type] = []
+        # analyses[current_analyse.type].append(current_analyse)
+
+        # parse data to find changes
+
+    # # accurracy comparison plot
+    # plt.figure()
+    # for graph_type in range(len(analyses)):
+    #     curve = []
+    #     for analyse in analyses[graph_type]:
+    #         curve.append( (analyse.num_agents, analyse.getAccuracy()) )
+    #     curve.sort(key = lambda point: point[0])
+    #     # ploting this curve with seaborn
+    #     X = [point[0] for point in curve]
+    #     Y = [point[1] for point in curve]
+    #     plt.plot(X, Y, label=GRAPH_TYPE[graph_type])
+    # plt.title("accuracy vs graph size and graph type")
+    # plt.legend()
+    # plt.xlabel('Number of agents')
+    # plt.ylabel('accuracy (%)')
+    # plt.grid(True)
+    # plot_path = Path(analyse_path + 'accuracy_vs_number_of_agents.png')
+    # plt.savefig(plot_path)
+    # plt.close()
+
+    # opinion change analyse
+
