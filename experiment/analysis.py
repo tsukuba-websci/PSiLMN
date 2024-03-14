4d5a59847d39e04f9a016166032cf5f6033ffa2c
Alexandre Gournail <alexandre.gournail@grenoble-inp.org>
2024-03-14 11:08:04 +0900

    add consensus and accurracy vs round, update gitignore

--- experiment/analysis.py
+++ experiment/analysis.py
@@ -20,21 +20,6 @@ GRAPH_TYPE = ['fully_connected_network',
             'random_network',
             'watts_strogatz_network']
 
-# class OpinionEvolution:
-#     """ 
-#     Data structure to save an agent opinion between two round.
-#     Round in [2, 3].
-#     """
-#     def __init__(self, previous : str, new : str, correct : str, round : int, agent_num: int) -> None:
-#         self.previous_opinion = previous
-#         self.new_opinion = new
-#         self. correct_response = correct
-#         self.round = round
-#         self.agent = agent_num
-
-#     def isAGoodChange(self) -> bool:
-#         return self.response == self.new_opinion
-
 class Analyse:
     """Meta data for a given graph and size"""
 
@@ -44,16 +29,25 @@ class Analyse:
         self.num_agents = int(csv_path.split('/')[3].split('.')[0])
         self.path = csv_path
         self.__accuracy = None
+        self.__consensus = None
 
     def setAccuracy(self) -> None:
         self.__accuracy = calculate_accuracy(self.path)
 
+    def setConsensus(self) -> None:
+        self.__consensus = calculate_consensus(self.path)
 
-    def getAccuracy(self) -> int:
+    def getAccuracy(self) -> list[int]:
         # We check if it as already been computed
-        if not self.__accuracy:
+        if self.__accuracy is None:
             self.setAccuracy()
         return self.__accuracy
+
+    def getConsensus(self) -> pd.DataFrame:
+        # We check if it as already been computed
+        if self.__consensus is None:
+            self.setConsensus()
+        return self.__consensus
 
     def ParseOpinionEvolution(self) -> pd.DataFrame :
         return find_evolutions(self.path)
@@ -112,40 +106,28 @@ def created_figs(file_path: str) -> None:
         for image_path in images:
             os.remove(image_path)
 
-# Function to calculate the accuracy of group responses
-def calculate_accuracy(file_path):
+# Function to calculate the accuracy of group responses for each round
+def calculate_accuracy(file_path: str) -> pd.DataFrame:
     # Read the CSV file
     df = pd.read_csv(file_path, delimiter='|')
-    
-    # Group by question number and find the final round for each question
-    final_rounds = df.groupby('question_number')['round'].max().reset_index()
-    
-    correct_answers = 0
-    total_questions = final_rounds.shape[0]
+    df['parsed_response'] = df['response'].apply(parse_response_mmlu)
+    df['correct'] = df['parsed_response'] == df['correct_response']
 
-    for _, row in final_rounds.iterrows():
-        question_number = row['question_number']
-        final_round = row['round']
-        
-        # Filter responses for the final round of this question
-        final_responses = df[(df['question_number'] == question_number) & (df['round'] == final_round)].copy()
+    num_agent = df['agent_id'].unique().size
 
-        # Correctly setting values to avoid SettingWithCopyWarning
-        final_responses.loc[:, 'parsed_response'] = final_responses['response'].apply(parse_response_mmlu)
+    # We select only the correct responses and remove unecessary columns 
+    accuracy_per_round = df[['round', 'question_number', 'correct']].query('correct')
 
-        # Find the most common response
-        most_common_response = Counter(final_responses['parsed_response']).most_common(1)[0][0]
-        
-        # Get the correct response (assuming it's the same for all rows of the same question)
-        correct_response = final_responses['correct_response'].iloc[0]
-        
-        # Check if the most common response matches the correct response
-        if most_common_response == correct_response:
-            correct_answers += 1
-    
-    # Calculate and return the accuracy percentage
-    accuracy_percentage = (correct_answers / total_questions) * 100
-    return accuracy_percentage
+    # We count correct answers for each line
+    accuracy_per_round = accuracy_per_round.groupby(['round', 'question_number']).count().reset_index()
+
+    # The network gives the right answer to a question if count > num agent / 2 
+    accuracy_per_round['accuracy'] = accuracy_per_round['correct'].apply(lambda correct_num : 1 if correct_num > num_agent / 2 else 0)
+
+    # Then we compute the average accuracy (for each round)
+    accuracy_per_round = accuracy_per_round[['round', 'accuracy']].groupby('round').mean()
+
+    return accuracy_per_round
 
 def find_evolutions(file_path) -> pd.DataFrame :
     opinion_evol_list = [] # list to be turned into a dataframe
@@ -156,10 +138,10 @@ def find_evolutions(file_path) -> pd.DataFrame :
 
     for id in df['agent_id'].unique():
         for question in df['question_number'].unique():
-            for round in [0,1]:
-                prev = df.query(f'agent_id == {id} & question_number == {question} & round == {round}')
+            for round in [1,2]:
+                prev = df.query(f'agent_id == {id} & question_number == {question} & round == {round-1}')
                 assert len(prev.axes[0]) == 1
-                next = df.query(f'agent_id == {id} & question_number == {question} & round == {round+1}')
+                next = df.query(f'agent_id == {id} & question_number == {question} & round == {round}')
                 assert len(next.axes[0]) == 1
 
                 # Here we determine wich type of evolution we have : 00, 01, 10 or 11,
@@ -173,8 +155,25 @@ def find_evolutions(file_path) -> pd.DataFrame :
                     type = type+"1"
                 else :
                     type = type+"0"
-                opinion_evol_list.append([id, str(round + 1), type])
+                opinion_evol_list.append([id, str(round), type])
     return pd.DataFrame(columns = ['agent_id', 'round', 'type'], data=opinion_evol_list)   
+
+def calculate_consensus(file_path: str) -> pd.DataFrame :
+    # Read the CSV file
+    df = pd.read_csv(file_path, delimiter='|')
+    df['parsed_response'] = df['response'].apply(parse_response_mmlu)
+    df['correct'] = df['parsed_response'] == df['correct_response']
+
+    num_agent = df['agent_id'].unique().size
+
+    # We select only the correct responses in the last round and remove unecessary columns 
+    final_consensus = df.query('correct & round == 2')[['question_number', 'correct']]
+
+    # We count the proportion of agent with a correct answers for each question
+    final_consensus = final_consensus.groupby(['question_number']).count().reset_index()
+    final_consensus['consensus'] = final_consensus['correct'].apply(lambda correct : correct/num_agent)
+
+    return final_consensus[['question_number', 'consensus']]
 
 if __name__ == "__main__":
     
@@ -182,71 +181,109 @@ if __name__ == "__main__":
     output_analyse_path = "output/analysis/"
     Path(output_analyse_path).mkdir(parents=True, exist_ok=True)
 
-    # Specify the path to your CSV file
+    # Specify the path to your CSV files
     csv_files = glob.glob('output/agent_responses/**/*.csv', recursive=True)
 
-    # analyses[i] contains the analyses relatives to a graph of type i
+    # analyses[i] is a list wich contain analyses relative to graphs of type i
     analyses = [None] * len(GRAPH_TYPE)
 
-    # Print the list of files
+    # reset result files and write headers
+    f = open(f'{output_analyse_path}accuracy.csv', 'w', newline='')
+    writer = csv.writer(f)
+    writer.writerow(['graph_type', 'size', 'accuracy round 1', 'accuracy round 2', 'accuracy round 3'])
+    f.close()
+
+    # Loop on each csv file
     for file in csv_files:
         print(file)
         current_analyse = Analyse(file) 
         
+        # Create figs for each simulation
         # created_figs(file)
 
-        # # Calculate the accuracy of group responses
-        # accuracy = current_analyse.getAccuracy()
+        # Calculate the accuracy of group responses
+        accuracy = current_analyse.getAccuracy()
 
-        # # # save accurracy
-        # with open('output/analysis/accuracy.csv', 'a', newline='') as result_file:
-        #     writer = csv.writer(result_file)
-        #     # Check if the file is empty to write headers
-        #     if result_file.tell() == 0:
-        #         writer.writerow(['graph_type', 'size', 'accuracy'])
-        #     writer.writerow([GRAPH_TYPE[current_analyse.type], current_analyse.num_agents, accuracy])
+        # save accuracy
+        with open(f'{output_analyse_path}accuracy.csv', 'a', newline='') as result_file:
+            writer = csv.writer(result_file)
+            writer.writerow([GRAPH_TYPE[current_analyse.type],
+                             current_analyse.num_agents,
+                             accuracy.query('round == 0').values[0][0],
+                             accuracy.query('round == 1').values[0][0],
+                             accuracy.query('round == 2').values[0][0]])
+            
+        # Calculate the consensus for this graph
+        consensus = current_analyse.getConsensus()
 
-        # Parse the opinion evolution list and display it
-        opinion_evol = current_analyse.ParseOpinionEvolution()
-        plt.figure()
-        sns.histplot(data=opinion_evol, x="round", hue="type", multiple="dodge", shrink=.8, stat="count")
-        plt.grid()
-        plt.xlabel("round number")
-        plt.ylabel("number of agents")
-        plt.title(f"opinion changes during the round for {current_analyse.num_agents} agents \nin a {GRAPH_TYPE[current_analyse.type]}")
+        # create the result path or reset if it already exists
+        result_file_path = f'{output_analyse_path}consensus/{GRAPH_TYPE[current_analyse.type]}/'
+        Path(result_file_path).mkdir(parents=True, exist_ok=True)
 
-        # Save the fig
-        file_path = f'{output_analyse_path}/opinion_changes/{GRAPH_TYPE[current_analyse.type]}/'
-        Path(file_path).mkdir(parents=True, exist_ok=True)
-        plt.savefig(Path(f'{file_path}/{current_analyse.num_agents}.png'))
+        # Draw the plot and save it
+        plt.figure(figsize=(16, 9))
+        sns.displot(consensus, x="consensus")
+        plt.title(f"average consensus per question for {current_analyse.num_agents} agents\nin {GRAPH_TYPE[current_analyse.type]}")
+        plt.xlabel("consensus (proportion of correct answers)")
+        plt.ylabel("frequence (%)")
+        plt.savefig(f'{result_file_path}{current_analyse.num_agents}.png')
         plt.close()
 
-        # # add analyse to the list for later
-        # if not (analyses[current_analyse.type]):
-        #     analyses[current_analyse.type] = []
-        # analyses[current_analyse.type].append(current_analyse)
+        # save the csv file
+        consensus.to_csv(f'{result_file_path}{current_analyse.num_agents}.csv', mode='w', sep=',', index=False)
 
-        # parse data to find changes
+        # # Parse the opinion evolution list and display it
+        # opinion_evol = current_analyse.ParseOpinionEvolution()
+        # plt.figure()
+        # sns.histplot(data=opinion_evol, x="round", hue="type", multiple="dodge", shrink=.8, stat="count")
+        # plt.grid()
+        # plt.xlabel("round number")
+        # plt.ylabel("number of agents")
+        # plt.title(f"opinion changes during the round for {current_analyse.num_agents} agents \nin a {GRAPH_TYPE[current_analyse.type]}")
 
-    # # accurracy comparison plot
-    # plt.figure()
-    # for graph_type in range(len(analyses)):
-    #     curve = []
-    #     for analyse in analyses[graph_type]:
-    #         curve.append( (analyse.num_agents, analyse.getAccuracy()) )
-    #     curve.sort(key = lambda point: point[0])
-    #     # ploting this curve with seaborn
-    #     X = [point[0] for point in curve]
-    #     Y = [point[1] for point in curve]
-    #     plt.plot(X, Y, label=GRAPH_TYPE[graph_type])
-    # plt.title("accuracy vs graph size and graph type")
-    # plt.legend()
-    # plt.xlabel('Number of agents')
-    # plt.ylabel('accuracy (%)')
-    # plt.grid(True)
-    # plot_path = Path(analyse_path + 'accuracy_vs_number_of_agents.png')
-    # plt.savefig(plot_path)
-    # plt.close()
+        # # Save the fig
+        # file_path = f'{output_analyse_path}/opinion_changes/{GRAPH_TYPE[current_analyse.type]}/'
+        # Path(file_path).mkdir(parents=True, exist_ok=True)
+        # plt.savefig(Path(f'{file_path}/{current_analyse.num_agents}.png'))
+        # plt.close()
 
-    # opinion change analyse
+        # add analyse to the list for later
+        if not (analyses[current_analyse.type]):
+            analyses[current_analyse.type] = []
+        analyses[current_analyse.type].append(current_analyse)
 
+    # accuracy comparison plot
+    # number of agent vs accuracy
+    plt.figure(figsize=(16, 9))
+    for graph_type in range(len(analyses)):
+        curve = []
+        for analyse in analyses[graph_type]:
+            curve.append( (analyse.num_agents, analyse.getAccuracy().query('round == 2').values[0][0]) )
+        curve.sort(key = lambda point: point[0])
+        # ploting this curve with seaborn
+        X = [point[0] for point in curve]
+        Y = [point[1] for point in curve]
+        plt.plot(X, Y, label=GRAPH_TYPE[graph_type])
+    plt.title("accuracy vs graph size and graph type")
+    plt.legend()
+    plt.xlabel('Number of agents')
+    plt.ylabel('accuracy (%)')
+    plt.grid(True)
+    plot_path = Path(output_analyse_path + 'accuracy_vs_number_of_agents.png')
+    plt.savefig(plot_path)
+    plt.close()
+
+    # round vs accuracy
+    plt.figure(figsize=(16, 9))
+    for graph_type in range(len(analyses)):
+        for analyse in analyses[graph_type]:
+            Y = analyse.getAccuracy()
+            plt.plot(['1', '2', '3'], Y, label=f'{GRAPH_TYPE[graph_type]}_{analyse.num_agents}')
+    plt.title("accuracy vs round number and graph type")
+    plt.legend()
+    plt.xlabel('Round number')
+    plt.ylabel('accuracy (%)')
+    plt.grid(True)
+    plot_path = Path(output_analyse_path + 'accuracy_vs_round.png')
+    plt.savefig(plot_path)
+    plt.close()
